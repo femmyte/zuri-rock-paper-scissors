5239ed4c12079dd7baea5be317da045029e18478
femmyte <sanyaoluadefemi@gmail.com>
2022-06-09 21:01:28 +0100

    rectified some errors in the program

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
+
 import time
+
 option = ['R', 'P', 'S']
 
 
@@ -8,56 +10,49 @@ def printDelay(message):
     print(message)
 
 
-def init():
-    movement()
-    user_move()
+def playerMove():
+    player = input('Choose from the option to play \n "R" '
+                   'for "rock", "P" for "paper", "S" for "scissors"').upper()
+    # if player in option:
+    #     print(player)
+    # else:
+    #     print('Invalid movement, try again')
+    #     playerMove()
+    return player
 
 
-def user_move():
-    move = input('Choose from the option to play \n "R" '
-                 'for "rock", "P" for "paper", "S" for "scissors"').upper()
-
-    return move
-
-
-computer_move = ''
-# comp_move = random.choice(option)
-comp_move = 'R'
-if comp_move == 'R':
-    computer_move = 'Rock'
-elif comp_move == 'S':
-    computer_move = 'Scissors'
-else:
-    computer_move = 'Paper'
-
-# user_move = user()
+def computerMove():
+    computer = random.choice(option)
+    return computer
 
 
 def score(player, computer):
-    if(player == 'R' and computer == 'R') or (player == 'S' and computer == 'S') or (player == 'P' and computer == 'P'):
-        print('Tie')
-        user_move()
-    elif(player == 'R' and computer == 'S') or (player == 'P' and computer == 'R') or (player == 'S' and computer == 'P'):
-        return 'Player Won'
-    elif(computer == 'R' and player == 'S') or (computer == 'P' and player == 'R') or (computer == 'S' or player == 'S'):
-        return 'Computer Won'
+    if ((player == 'R' and computer == 'S') or
+        (player == 'S' and computer == 'P') or
+            (player == 'P' and computer == 'R')):
+        print("** YOU WON **")
+    elif ((computer == 'R' and player == 'S') or
+            (computer == 'S' and player == 'P') or
+            (computer == 'P' and player == 'R')):
+        print("** COMPUTER WON **")
+    elif ((player == 'R' and computer == 'R') or
+            (player == 'S' and computer == 'S') or
+            (player == 'P' and computer == 'P')):
+        print("** TIE **")
 
 
-while user_move() not in option:
-    printDelay('Invalid move kindly enter the right option')
-    user_move()
+def init():
+    player = playerMove()
+    while player not in option:
+        print('Invalid movement, try again')
+        playerMove()
+        continue
 
-
-def movement():
-    if(user_move() == 'R'):
-        printDelay(f'Player (Rock) : Computer ({computer_move})')
-        print(score(user_move(), comp_move))
-    elif(user_move() == 'P'):
-        printDelay(f'Player (Paper) : Computer ({computer_move})')
-        print(score(user_move(), comp_move))
-    elif(user_move() == 'S'):
-        printDelay(f'Player (Scissors) : Computer ({computer_move})')
-        print(score(user_move(), comp_move))
+    print(player)
+    computer = computerMove()
+    score(player, computer)
+    # if player in option:
+    # else:
 
 
 init()
